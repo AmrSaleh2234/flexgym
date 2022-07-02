e5fdbed26700770c87e73ac7a506eefaac4922f8
Amr Saleh <52182013+AmrSaleh2234@users.noreply.github.com>
2022-07-02 15:13:40 +0200

    edit button

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -53,8 +53,8 @@
 
                         <div class="row mb-0">
                             <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="" style="color: rgb(255, 255, 255) !important;">
-                                    {{ __('Login') }}
+                                <button type="submit" class="main-btn d-flex align-items-center" style="color: rgb(255, 255, 255) !important;height: 45px">
+                                  Login
                                 </button>
 
                                 @if (Route::has('password.request'))
