091d62659ebc3fc98e7e397f47c86d84227daa94
Amr Saleh <52182013+AmrSaleh2234@users.noreply.github.com>
2022-07-02 11:10:49 +0200

    delte element navbar

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -52,7 +52,6 @@
                     <ul class="nav">
                         <li class="scroll-to-section"><a href="#top" class="active">Home</a></li>
                         <li class="scroll-to-section"><a href="#features">About</a></li>
-                        <li class="scroll-to-section"><a href="#our-classes">Classes</a></li>
                         <li class="scroll-to-section"><a href="#schedule">Schedules</a></li>
                         <li class="scroll-to-section"><a href="#contact-us">Contact</a></li>
                         @if(auth()->check())
