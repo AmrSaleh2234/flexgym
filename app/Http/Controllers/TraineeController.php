5b4c77455b0f9060e488b01ea82b7c134f3c6476
Amr Saleh <52182013+AmrSaleh2234@users.noreply.github.com>
2022-10-09 19:16:47 +0200

    .

--- app/Http/Controllers/TraineeController.php
+++ app/Http/Controllers/TraineeController.php
@@ -177,7 +177,7 @@ class TraineeController extends Controller
         $data = Trainee::where(function ($query)use($search)
         {
             $query->where('name', 'LIKE', '%' . $search . '%')->orWhere('id','LIKE',  $search );
-        })->paginate(50)
+        })->paginate(50);
         return view('trainee.all', compact('data'));
     }
     public function searchForMembers()
